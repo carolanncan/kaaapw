fde21df1085363534f02bc2e823d10377c1d3407
Carolann Bonner <carolanncan@gmail.com>
2014-05-14 19:21:05 -0700

    Set relative links in config

--- config.rb
+++ config.rb
@@ -75,20 +75,14 @@ page "/feed.xml", layout: false
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
+set :relative_links, true
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
   # activate :minify_css
-
-  # Minify Javascript on build
   # activate :minify_javascript
-
-  # Enable cache buster
   # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"

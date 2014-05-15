c1b05eac56c133c5cd218cf026f2723e74ced53f
carolanncan <carolanncan@gmail.com>
2014-05-14 21:46:18 -0700

    Transfer finding type source files

---
+++ source/javascripts/sliding_menu.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  $('.js-menu-trigger').on('click touchstart', function(e){
+    $('.sliding-menu-button').toggleClass('is-visible');
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+
+  $('.js-menu-screen').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+});
---
+++ source/posts/2014-04-20-a-brand-new-post.html.markdown
@@ -0,0 +1,14 @@
+---
+layout: posts
+title: A Brand New Post
+subtitle: Blogging All Things
+date: 2014-04-20
+image: silence.jpg
+location: Oakland
+tags: handlettering, street art
+---
+
+![papers][papers]
+I found this specimen in East Bay on my walk home from the BART Ashby station.
+
+[papers]:papers.jpg
